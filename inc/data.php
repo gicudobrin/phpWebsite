366c891ecb0690fde424cc6b82cd34e57cf938bb
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-11-26 23:12:42 +0200

    Add files to the repo

---
+++ inc/data.php
@@ -0,0 +1,178 @@
+<?php
+$catalog = [];
+$catalog[101] = [
+	"title" => "A Design Patterns: Elements of Reusable Object-Oriented Software",
+	"img" => "img/media/design_patterns.jpg",
+    "genre" => "Tech",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "Erich Gamma",
+        "Richard Helm",
+        "Ralph Johnson",
+        "John Vlissides"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+    "title" => "Clean Code: A Handbook of Agile Software Craftsmanship",
+    "img" => "img/media/clean_code.jpg",
+    "genre" => "Tech",
+    "format" => "Ebook",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Robert C. Martin"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+    "title" => "Refactoring: Improving the Design of Existing Code",
+    "img" => "img/media/refactoring.jpg",
+    "genre" => "Tech",
+    "format" => "Hardcover",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Martin Fowler",
+        "Kent Beck",
+        "John Brant",
+        "William Opdyke",
+        "Don Roberts"
+    ],
+    "publisher" => "Addison-Wesley Professional",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "The Clean Coder: A Code of Conduct for Professional Programmers",
+    "img" => "img/media/clean_coder.jpg",
+    "genre" => "Tech",
+    "format" => "Audio",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Robert C. Martin"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Mihai Viteazul",
+    "img" => "img/media/mihai.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1970,
+    "category" => "Movies",
+    "director" => "Sergiu Nicolaescu",
+    "writers" => [
+        "Titus Popovici",
+    ],
+    "stars" => [
+        "Amza Pellea",
+        "Ion Besoiu",
+        "Sergiu Nicolaescu",
+        "Olga Tudorache",
+    ]
+];
+$catalog[202] = [
+    "title" => "Dacii",
+    "img" => "img/media/dacii.jpg",
+    "genre" => "History",
+    "format" => "Blu-ray",
+    "year" => 1966,
+    "category" => "Movies",
+    "director" => "Sergiu Nicolaescu",
+    "writers" => [
+        "Titus Popovici",
+    ],
+    "stars" => [
+        "Pierre Brice",
+        "Amza Pellea",
+        "Mircea Albulescu",
+        "Marie-José Nat",
+    ]
+];
+$catalog[203] = [
+    "title" => "Nemuritorii",
+    "img" => "img/media/nemuritorii.jpg",
+    "genre" => "Drama",
+    "format" => "Blu-ray",
+    "year" => 1974,
+    "category" => "Movies",
+    "director" => "Sergiu Nicolaescu",
+    "writers" => [
+        "Sergiu Nicolaescu",
+    ],
+    "stars" => [
+        "Sergiu Nicolaescu",
+        "Amza Pellea",
+        "Ion Besoiu",
+        "Ilarion Ciobanu",
+        "Jean Constantin",
+        "Colea Rautu",
+    ]
+];
+$catalog[204] = [
+    "title" => "Nea Marin Miliardar",
+    "img" => "img/media/marin.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 1979,
+    "category" => "Movies",
+    "director" => "Sergiu Nicolaescu",
+    "writers" => [
+        "Eugen Barbu",
+        "Eugen Burada",
+        "Vintila Corbul",
+        "Amza Pellea",
+    ],
+    "stars" => [
+        "Amza Pellea",
+        "Jean Constantin",
+        "Sebastian Papaiani",
+        "Stefan Banica",
+        "Stela Popescu",
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Beethoven: Complete Symphonies",
+    "img" => "img/media/beethoven.jpg",
+    "genre" => "Clasical",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Ludwig van Beethoven"
+];
+$catalog[302] = [
+    "title" => "Elvis Forever",
+    "img" => "img/media/elvis_presley.jpg",
+    "genre" => "Rock",
+    "format" => "Vinyl",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "Elvis Presley"
+];
+$catalog[303] = [
+    "title" => "No Fences",
+    "img" => "img/media/garth_brooks.jpg",
+    "genre" => "Country",
+    "format" => "Cassette",
+    "year" => 1990,
+    "category" => "Music",
+    "artist" => "Garth Brooks"
+];
+$catalog[304] = [
+    "title" => "The Very Thought of You",
+    "img" => "img/media/nat_king_cole.jpg",
+    "genre" => "Jaz",
+    "format" => "MP3",
+    "year" => 2008,
+    "category" => "Music",
+    "artist" => "Nat King Cole"
+];
+?>
